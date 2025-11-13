3313e5dcf4b2b16f075dadc26a6b3518f32b4577
GbPiamolini <gpiupiu01@gmail.com>
2025-11-13 11:11:45 -0300

    Update app.js

--- alugames/js/app.js
+++ alugames/js/app.js
@@ -4,13 +4,16 @@ function alterarStatus(id) {
     let botao = botaoGame.querySelector('.dashboard__item__button');
 
     if (botao.classList.contains('dashboard__item__button--return')) {
+        if (confirm(`Você tem certeza que quer devolver o jogo ${nomeJogo.textContent}?`)) {
         botao.classList.remove('dashboard__item__button--return');
         imagem.classList.remove('dashboard__item__img--rented');
         botao.textContent = 'Alugar';
+        }
     } else {
         botao.classList.add('dashboard__item__button--return');
         imagem.classList.add('dashboard__item__img--rented');
         botao.textContent = 'Devolver'
     }
+
 
 }
