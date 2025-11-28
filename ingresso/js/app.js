9124cc645b7193413eeb051737bcf9eb9772a074
GbPiamolini <gpiupiu01@gmail.com>
2025-11-28 11:18:08 -0300

    Add files via upload

---
+++ ingresso/js/app.js
@@ -0,0 +1,8 @@
+let pista = document.getElementById('qtd-pista').value;
+let cadeiraSuperior = document.getElementById('qtd-superior').value;
+let cadeiraInferior = document.getElementById('qtd-inferior').value;
+
+function comprar() {
+    let quantidade = document.getElementById('qtd').value;
+    alert (pista);
+}
