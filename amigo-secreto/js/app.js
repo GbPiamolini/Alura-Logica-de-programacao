ec5867d77d8582a66699f186858a27f111fd2eb3
GbPiamolini <gpiupiu01@gmail.com>
2025-12-12 09:56:23 -0300

    Add files via upload

---
+++ amigo-secreto/js/app.js
@@ -0,0 +1,60 @@
+let listaAmigo = [];
+
+function adicionar() {
+    let nomeAmigo = document.getElementById('nome-amigo');
+    let lista = document.getElementById('lista-amigos');  
+    if (nomeAmigo.value == '') {
+        alert ('Insira um nome valido')
+    } else {
+    if(listaAmigo.includes(nomeAmigo.value)) {
+        alert('Esse nome ja está incluido');
+        nomeAmigo.value = '';
+    } else {
+        
+        listaAmigo.push(nomeAmigo.value);
+        
+        if (lista.textContent == '') {
+            lista.textContent = nomeAmigo.value;
+        } else {
+            lista.textContent = lista.textContent + ', ' + nomeAmigo.value; 
+        }
+
+    nomeAmigo.value = '';
+    }
+}
+}
+
+function embaralha(lista) {
+
+    for (let indice = lista.length; indice; indice--) {
+
+        const indiceAleatorio = Math.floor(Math.random() * indice);
+
+        // atribuição via destructuring
+        [lista[indice - 1], lista[indiceAleatorio]] = 
+            [lista[indiceAleatorio], lista[indice - 1]];
+    }
+}
+
+function sortear() {
+    embaralha(listaAmigo);
+    let listaSorteio = document.getElementById('lista-sorteio');
+    
+    if (listaAmigo.length <= 4) {
+        alert('Quantidade de pessoas insuficientes');
+    } else {
+        for (let i = 0; i < listaAmigo.length; i++) {
+            if (i == listaAmigo.length - 1) {
+                listaSorteio.innerHTML = listaSorteio.innerHTML + listaAmigo[i] + ' --> ' + listaAmigo[0] + '<br>';
+            } else {
+                listaSorteio.innerHTML = listaSorteio.innerHTML + listaAmigo[i] + ' --> ' + listaAmigo[i + 1] + '<br>';
+            } 
+        }
+    }
+}
+
+function reiniciar() { 
+    listaAmigo = [];
+    document.getElementById('lista-sorteio').innerHTML = '';
+    document.getElementById('lista-amigos').innerHTML = '';
+}
